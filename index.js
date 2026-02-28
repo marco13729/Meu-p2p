d4ab14dd68ce9004e361f31f44bf6bcb50c0667b
marco13729 <mac21022001@gmail.com>
2026-02-28 09:41:18 -0300

    Initialize Express server with proxy functionality

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+const TARGET = process.env.TARGET;
+const USER = process.env.USER;
+const PASS = process.env.PASS;
+
+app.get('*', async (req, res) => {
+    try {
+        const url = `${TARGET}${req.url.replace('username=88778851', `username=${USER}`).replace('password=cspro', `password=${PASS}`)}`;
+        const response = await axios.get(url, { responseType: 'arraybuffer' });
+        res.set(response.headers);
+        res.send(response.data);
+    } catch (e) {
+        res.status(500).send("Erro no servidor P2P");
+    }
+});
+
+app.listen(process.env.PORT || 3000, () => console.log("Servidor Ativo"));
