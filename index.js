9805db1417a4a1e58e2ebd3a24fb0ee5f10c50a1
marco13729 <mac21022001@gmail.com>
2026-02-28 10:38:07 -0300

    Refactor error message and streamline server log

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-
 const TARGET = process.env.TARGET || '';
 const USER = process.env.USER || '';
 const PASS = process.env.PASS || '';
@@ -9,14 +8,12 @@ const PASS = process.env.PASS || '';
 app.get('*', async (req, res) => {
     try {
         const finalUrl = `${TARGET}${req.url}`.replace('username=88778851', `username=${USER}`).replace('password=cspro', `password=${PASS}`);
-        const response = await axios.get(finalUrl, { responseType: 'arraybuffer', timeout: 15000 });
+        const response = await axios.get(finalUrl, { responseType: 'arraybuffer' });
         res.set(response.headers);
         res.send(response.data);
     } catch (e) {
-        res.status(500).send("Erro de Conexão");
+        res.status(500).send("Erro");
     }
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("SERVIDOR_ONLINE");
-});
+app.listen(process.env.PORT || 3000, () => console.log("SERVIDOR_ONLINE"));
